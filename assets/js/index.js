ad64790aa53d2ef17f66d67ad1d17c05ee79b3c6
ShkurtaH <sh51844@ubt-uni.net>
2021-12-26 13:53:05 +0100

    Final commit

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+// window.onscroll = function(event) {
+//     //
+//     if(window.scrollTo > 50){
+//         document.getElementsByClassName("header").classList.add("sticky");
+//     }
+//     else{
+//         document.getElementsByClassName("header").classList.remove("sticky");
+//     }
+// };
