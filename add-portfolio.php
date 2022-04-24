32cdbe6708346ff0d39719d6d4e0ac843a09cfc0
ShkurtaH <sh51844@ubt-uni.net>
2022-04-24 10:08:00 +0200

    backend portfolio crud validation

--- add-portfolio.php
+++ add-portfolio.php
@@ -3,17 +3,24 @@ include("layout/header.php");
 include_once("Crud.php");
 
 $crud = new crud();
+$image = "";
+$image_err = "";
 
 if (isset($_POST['submit'])) {
-    $data = array(
-        "image" => $crud->escape_string($_POST['image']),
-    );
-    $crud->insert($data, 'portfolio');
-    if ($data) {
-        echo 'insert successfully';
-        header('location:portfolio-list.php');
-    } else {
-        echo 'try again';
+    if (empty(trim($_POST["image"]))) {
+        $image_err = "Please upload an image";
+    }
+    if (empty($image_err)) {
+        $data = array(
+            "image" => $crud->escape_string($_POST['image']),
+        );
+        $crud->insert($data, 'portfolio');
+        if ($data) {
+            echo 'insert successfully';
+            header('location:portfolio-list.php');
+        } else {
+            echo 'try again';
+        }
     }
 }
 session_start();
@@ -35,6 +42,7 @@ if (isset($_SESSION['username']) && isset($_SESSION['id'])) { ?>
                 <h4>Add new item to portfolio gallery</h4>
                 <div class="form-group">
                     <input type="file" class="main-input" name="image" placeholder="Image">
+                    <div id="portfolio-image-error" class="text-error"><?php echo $image_err; ?></div>
                 </div>
                 <div class="form-group">
                     <input type="submit" name="submit" class="btn" value="Save">
--- edit-portfolio.php
+++ edit-portfolio.php
@@ -1,27 +1,29 @@
 <?php
-include ("layout/header.php");
+include("layout/header.php");
 include_once("Crud.php");
 
 $id = $_GET['editid'];
 
-$crud= new crud();
+$crud = new crud();
+$image = "";
+$image_err = "";
+$data = $crud->selectbyid('portfolio', $id);
 
-$data = $crud->selectbyid('portfolio',$id);
-
-if(isset($_POST['submit']))
-{
-    $data= array(
-        "image"  => $crud->escape_string($_POST['image']),
-    );
-    $crud->update($data,'portfolio',$id);
-    if($data)
-    {
-        echo 'updated successfully';
-        header('location:portfolio-list.php');
+if (isset($_POST['submit'])) {
+    if (empty(trim($_POST["image"]))) {
+        $image_err = "Please upload an image";
     }
-    else
-    {
-        echo 'try again' ;
+    if (empty($image_err)) {
+        $data = array(
+            "image" => $crud->escape_string($_POST['image']),
+        );
+        $crud->update($data, 'portfolio', $id);
+        if ($data) {
+            echo 'updated successfully';
+            header('location:portfolio-list.php');
+        } else {
+            echo 'try again';
+        }
     }
 }
 
@@ -29,30 +31,30 @@ session_start();
 if (isset($_SESSION['username']) && isset($_SESSION['id'])) { ?>
 <nav>
     <?php if ($_SESSION['role'] == 'admin') { ?>
-    <!-- For Admin -->
-    <div class="admin-top-header flex custom-justify">
-        <figure class="admin-logo">
-            <img src="assets/images/general/Shkurta-Photographer.jpg" alt="" title="">
-        </figure>
-        <div class="dashboard-btns">
-            <?= $_SESSION['username'] ?>
-            <a href="logout.php" class="btn">Logout</a>
+        <!-- For Admin -->
+        <div class="admin-top-header flex custom-justify">
+            <figure class="admin-logo">
+                <img src="assets/images/general/Shkurta-Photographer.jpg" alt="" title="">
+            </figure>
+            <div class="dashboard-btns">
+                <?= $_SESSION['username'] ?>
+                <a href="logout.php" class="btn">Logout</a>
+            </div>
         </div>
-    </div>
-    <div class="tabset">
-        <form method="POST" name="form">
-            <h4>Update "<?php echo $data['id'];?>" gallery item</h4>
-            <div class="form-group">
-                <input type="file" name="image" value="<?php echo $data['image'];?>" class="main-input" placeholder="Upload image">
-            </div>
-            <div class="form-group">
-                <input type="submit" name="submit" class="btn" value="Update">
-                <a href="portfolio-list.php" class="btn">Back to portfolio list</a>
-            </div>
-        </form>
-    </div>
-    <?php }
-    else { ?>
+        <div class="tabset">
+            <form method="POST" name="form">
+                <h4>Update "<?php echo $data['id']; ?>" gallery item</h4>
+                <div class="form-group">
+                    <input type="file" name="image" value="<?php echo $data['image']; ?>" class="main-input" placeholder="Upload image">
+                    <div id="edit-portfolio-image-error" class="text-error"><?php echo $image_err; ?></div>
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="submit" class="btn" value="Update">
+                    <a href="portfolio-list.php" class="btn">Back to portfolio list</a>
+                </div>
+            </form>
+        </div>
+    <?php } else { ?>
         <div class="admin-top-header flex custom-justify">
             <figure class="admin-logo">
                 <img src="assets/images/general/Vanesa-Photographer.jpg" alt="" title="">
@@ -63,7 +65,7 @@ if (isset($_SESSION['username']) && isset($_SESSION['id'])) { ?>
             </div>
         </div>
     <?php } ?>
-    <?php }else{
+    <?php } else {
         header("Location: index.php");
     } ?>
 </nav>
